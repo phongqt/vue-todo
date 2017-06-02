a9cbbc4ee5182a0cfdeb45dd76f49bd058aefb64
phong_tran_qt <vanphong12tlt@gmail.com>
2017-06-02 17:24:47 +0700

    Initial project

---
+++ src/components/TodoMain.vue
@@ -0,0 +1,45 @@
+<template>
+   <div>
+      <create-todo v-on:add-todo="addTodo"></create-todo>
+      <todo-list v-bind:todos="todos"></todo-list>
+   </div>
+</template>
+<script>
+  import TodoList from '@/components/TodoList'
+  import CreateTodo from '@/components/CreateTodo'
+  export default {
+    components: {
+      TodoList,
+      CreateTodo
+    },
+    data () {
+      return {
+        todos: [{
+          title: 'Todo A',
+          project: 'Project A',
+          done: false
+        }, {
+          title: 'Todo B',
+          project: 'Project B',
+          done: true
+        }, {
+          title: 'Todo C',
+          project: 'Project C',
+          done: false
+        }, {
+          title: 'Todo D',
+          project: 'Project D',
+          done: false
+        }]
+      }
+    },
+    methods: {
+      addTodo (title) {
+        this.todos.push({
+          title,
+          done: false
+        })
+      }
+    }
+  }
+</script>
